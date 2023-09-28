2d182dee105ac890358ce398eafdc1651b4c0358
stupidcupid135 <121999608+stupidcupid135@users.noreply.github.com>
2023-09-28 15:26:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const box = document.getElementById("box");
+let id = null;
+let pos = 0;
+clearInterval(id);
+let moving = false;
+box.innerHTML = "click me!";
+
+
+box.onclick = moveBox;
+
+function moveBox() {
+    moving = !moving;
+    if (moving) {
+        id = setInterval(frame, 5);
+        box.innerHTML = "";
+        box.style.backgroundColor = "green";
+    } else {
+        clearInterval(id);
+        box.style.backgroundColor = "red";
+    }
+}
+function frame() {
+    if (pos == 350) {
+        pos = 0;
+        clearInterval(id);
+        moving = false;
+        box.style.backgroundColor = "red";
+        box.innerHTML = "Done!";
+    } else if (moving) {
+        pos++;
+        box.style.top = pos + 'px';
+        box.style.left = pos + 'px';
+    }
+}
